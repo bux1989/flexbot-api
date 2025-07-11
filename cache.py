b139b99893025366089245859adea3988cea96c5
bux1989 <alexfbuckle@gmail.com>
2025-07-11 14:57:21 +0200

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,14 +1,27 @@
-# In utils.py or a new cache.py
 import time
+from threading import Lock
 
 _cache = {}
+_cache_lock = Lock()
 
 def cache_set(key, value, ttl=60):
-    _cache[key] = (value, time.time() + ttl)
+    """Set a cache entry with a time-to-live (in seconds)."""
+    with _cache_lock:
+        _cache[key] = (value, time.time() + ttl)
 
 def cache_get(key):
-    value, expires = _cache.get(key, (None, 0))
-    if time.time() > expires:
-        _cache.pop(key, None)
-        return None
-    return value
+    """Retrieve a cache entry if it hasn't expired; else return None."""
+    with _cache_lock:
+        entry = _cache.get(key)
+        if not entry:
+            return None
+        value, expires = entry
+        if time.time() > expires:
+            _cache.pop(key, None)
+            return None
+        return value
+
+def cache_clear():
+    """Clear all cached entries."""
+    with _cache_lock:
+        _cache.clear()
